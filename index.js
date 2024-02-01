ba8e793201b5b61b577220e96e691e4e5679aabc
Ashutoshbind15 <91549538+Ashutoshbind15@users.noreply.github.com>
2024-02-02 02:56:32 +0530

    add the label adder logic

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // index.js
 const core = require("@actions/core");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const github = require("@actions/github");
+
 const generativeAI = new GoogleGenerativeAI(process.env.GEMINI_PRO_API_KEY);
 
 const generateSingleTextResponse = async (
@@ -8,7 +10,7 @@ const generateSingleTextResponse = async (
 ) => {
   const model = generativeAI.getGenerativeModel({ model: "gemini-pro" });
   const result = await model.generateContent(
-    `Give me 3 labels for this github commit message: ${prompt}`
+    `Give me 3 labels for this github commit message as a comma separated string : ${prompt}`
   );
   const response = await result.response;
   const text = response.text();
@@ -20,6 +22,30 @@ const helper = async () => {
   console.log(`Commit Message: ${commitMessage}`);
   const labelString = await generateSingleTextResponse(commitMessage);
   console.log(labelString);
+
+  const token = process.env.GITHUB_TOKEN;
+  const octokit = github.getOctokit(token);
+
+  const context = github.context;
+  if (context.payload.pull_request == null) {
+    core.setFailed("No pull request found.");
+    return;
+  }
+
+  const prNumber = context.payload.pull_request.number;
+  const owner = context.repo.owner;
+  const repo = context.repo.repo;
+
+  const labels = labelString.split(",").map((label) => label.trim());
+
+  await octokit.rest.issues.addLabels({
+    owner,
+    repo,
+    issue_number: prNumber,
+    labels: labels,
+  });
+
+  console.log(`Labels added: ${labels}`);
 };
 
 helper().catch((error) => {
