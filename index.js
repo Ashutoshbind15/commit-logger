8e635d83d8aa259f83b89111e59c1279daf29013
Ashutoshbind15 <91549538+Ashutoshbind15@users.noreply.github.com>
2024-02-02 02:18:42 +0530

    add the logic for generating ai based tags

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 // index.js
 const core = require("@actions/core");
+const github = require("@actions/github");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const generativeAI = new GoogleGenerativeAI(process.env.GEMINI_PRO_API_KEY);
+
+const generateSingleTextResponse = async (
+  prompt = "Write a story about a magic backpack."
+) => {
+  const model = generativeAI.getGenerativeModel({ model: "gemini-pro" });
+  const result = await model.generateContent(
+    `Give me 3 labels for this github commit message: ${prompt}`
+  );
+  const response = await result.response;
+  const text = response.text();
+  return text;
+};
 
 try {
   // Get the commit message input from the workflow file
   const commitMessage = core.getInput("COMMIT_MESSAGE", { required: true });
+  const token = process.env.GITHUB_TOKEN;
+
+  const octokit = github.getOctokit(token);
+  const { context } = github;
+  const { owner, repo } = context.repo;
+
+  if (context.payload.pull_request == null) {
+    core.setFailed("No pull request found.");
+    return;
+  }
+
   console.log(`Commit Message: ${commitMessage}`);
+  console.log(`Labels: ${generateSingleTextResponse(commitMessage)}`);
 } catch (error) {
   core.setFailed(`Action failed with error ${error}`);
 }
